f8a3aef0625f8ed13ad1c003e27af54e8af5d18b
RobertBett <robertbett6@icloud.com>
2018-09-23 22:18:12 -0400

    first commit

---
+++ server/controllers/authentication.js
@@ -0,0 +1,39 @@
+const jwt = require('jwt-simple');
+const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user){
+    return jwt.encode({}, config.secret);
+}
+
+exports.signup = function(req, res, next){
+    const Email = req.body.email;
+    const Password = req.body.password;
+
+    if(!Email || !Password){
+        return res.status(422).send({ error: 'You must provide an email and a password'});
+    }
+
+   // See if a user with the given email exists
+   User.findOne({ email: Email}, (err, existingUser) =>{
+        if(err){ return next(err)}
+
+        // If a user with the email does exist, return an error
+        if(existingUser){
+            return res.status(422).send({error: 'Email is Already in Use'})
+        }
+        // If a user with the email does NOT exist, create and save user
+        const user = new User({
+            email: Email,
+            password: Password
+        })
+        user.save((err) =>{
+            if(err){ return next(err);}
+
+             // Respond to request initcating the user was created
+             res.json({ success : true});
+        })
+   });
+
+   
+}
