b8fffc1ca3008c279db820ae87fdc8f97337c462
RobertBett <robertbett6@icloud.com>
2018-10-15 00:26:40 -0400

    adds create account section

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,7 +4,8 @@ import classNames from 'classnames';
 import Paper from '@material-ui/core/Paper';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Avatar from '@material-ui/core/Avatar';
-import LockIcon from '@material-ui/icons/LockOutlined';
+import Lock from '@material-ui/icons/Lock';
+import LockOutlined from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import FormControl from '@material-ui/core/FormControl';
 import Button from '@material-ui/core/Button';
@@ -13,7 +14,9 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import PersonAdd from '@material-ui/icons/PersonAdd';
+import PersonOutlined from '@material-ui/icons/PersonAddOutlined';
 import AppBar from '@material-ui/core/AppBar';
+import '../index.css';
 
 const styles = theme => ({
   root: {
@@ -22,6 +25,20 @@ const styles = theme => ({
   },
   mainColor: {
     backgroundColor: '#f44336',
+  },
+  textColor: {
+    color: '#f44336',
+  },
+  cssLabel: {
+    '&$cssFocused': {
+      color: '#f44336',
+    },
+  },
+  cssFocused: {},
+  cssUnderline: {
+    '&:after': {
+      borderBottomColor: '#f44336',
+    },
   },
   layout: {
     width: 'auto',
@@ -90,13 +107,14 @@ class App extends React.Component {
       <main className={classNames(classes.layout, classes.margin)}>
         <AppBar position="static" color="inherit">
           <Tabs
+            className="MuiTab-textColorPrimary-59.MuiTab-selected-61"
             value={this.state.value}
             onChange={this.handleChange}
             fullWidth
-            indicatorColor="secondary"
-            textColor="secondary"
+            indicatorColor="primary"
+            textColor="primary"
           >
-            <Tab label="Sign in" icon={<LockIcon />} />
+            <Tab label="Sign in" icon={<Lock />} />
             <Tab label="Create an Account" icon={<PersonAdd />} />
           </Tabs>
         </AppBar>
@@ -104,23 +122,26 @@ class App extends React.Component {
           <TabContainer className={classes.margin}>
             <Paper className={classes.paper}>
               <Avatar className={classNames(classes.avatar, classes.margin)}>
-                <LockIcon className={classes.icon} />
+                <LockOutlined className={classes.icon} />
               </Avatar>
-              <Typography component="h3" variant="h5">
-            Sign in
+              <Typography component="h1" variant="h5">
+                Sign in
               </Typography>
               <form className={classes.form}>
                 <FormControl color="inherit" margin="normal" required fullWidth>
-                  <InputLabel htmlFor="email">Email Address</InputLabel>
-                  <Input id="email" name="email" autoComplete="email" autoFocus />
+                  <InputLabel FormLabelClasses={{ root: classes.cssLabel, focused: classes.cssFocused }} htmlFor="email">
+                  Email Address
+                  </InputLabel>
+                  <Input classes={{ underline: classes.cssUnderline }} id="email" name="email" autoComplete="email" autoFocus />
                 </FormControl>
                 <FormControl margin="normal" required fullWidth>
-                  <InputLabel htmlFor="password">Password</InputLabel>
+                  <InputLabel FormLabelClasses={{ root: classes.cssLabel, focused: classes.cssFocused }} htmlFor="password">Password</InputLabel>
                   <Input
                     name="password"
                     type="password"
                     id="password"
                     autoComplete="current-password"
+                    classes={{ underline: classes.cssUnderline }}
                   />
                 </FormControl>
                 <Button
@@ -137,7 +158,75 @@ class App extends React.Component {
             </Paper>
           </TabContainer>
         )}
-
+        {value === 1 && (
+          <TabContainer className={classes.margin}>
+            <Paper className={classes.paper}>
+              <Avatar className={classNames(classes.avatar, classes.margin)}>
+                <PersonOutlined />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Create an account
+              </Typography>
+              <form className={classes.form}>
+                <FormControl margin="normal" required>
+                  <InputLabel FormLabelClasses={{ root: classes.cssLabel, focused: classes.cssFocused }} htmlFor="firstname">First Name</InputLabel>
+                  <Input
+                    name="firstname"
+                    type="firstname"
+                    id="firstname"
+                    autoComplete="firstname"
+                    classes={{ underline: classes.cssUnderline }}
+                  />
+                </FormControl>
+                <FormControl margin="normal" required>
+                  <InputLabel FormLabelClasses={{ root: classes.cssLabel, focused: classes.cssFocused }} htmlFor="lastname">Last Name</InputLabel>
+                  <Input
+                    name="lastname"
+                    type="lastname"
+                    id="lastname"
+                    autoComplete="lastname"
+                    classes={{ underline: classes.cssUnderline }}
+                  />
+                </FormControl>
+                <FormControl color="inherit" margin="normal" required fullWidth>
+                  <InputLabel FormLabelClasses={{ root: classes.cssLabel, focused: classes.cssFocused }} htmlFor="email">
+                  Email Address
+                  </InputLabel>
+                  <Input classes={{ underline: classes.cssUnderline }} id="email" name="email" autoComplete="email" autoFocus />
+                </FormControl>
+                <FormControl margin="normal" required>
+                  <InputLabel FormLabelClasses={{ root: classes.cssLabel, focused: classes.cssFocused }} htmlFor="password">Password</InputLabel>
+                  <Input
+                    name="password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    classes={{ underline: classes.cssUnderline }}
+                  />
+                </FormControl>
+                <FormControl margin="normal" required>
+                  <InputLabel FormLabelClasses={{ root: classes.cssLabel, focused: classes.cssFocused }} htmlFor="password">Confirm Password</InputLabel>
+                  <Input
+                    name="password"
+                    type="password"
+                    id="confirmpassword"
+                    autoComplete="current-password"
+                    classes={{ underline: classes.cssUnderline }}
+                  />
+                </FormControl>
+                <Button
+                  type="submit"
+                  fullWidth
+                  size="medium"
+                  variant="extendedFab"
+                  color="primary"
+                  className={classNames(classes.submit, classes.mainColor)}
+                >
+                Create Account
+                </Button>
+              </form>
+            </Paper>
+          </TabContainer>)}
       </main>
     );
   }
