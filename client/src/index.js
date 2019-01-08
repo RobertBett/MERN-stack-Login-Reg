474da2f021f4c9f3daed04d35d4fd499b98b4f85
RobertBett <robertbett6@icloud.com>
2019-01-08 16:41:37 -0500

    fixes security vulnrabilities

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import thunk from 'redux-thunk';
 import App from './App';
 import reducers from './store/reducers';
@@ -14,10 +15,24 @@ const logger = store => next => (action) => {
   return result;
 };
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#f44336',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+  },
+});
+
 ReactDOM.render(
-  <Provider store={createStore(reducers, composeEnhancers(applyMiddleware(logger, thunk)))}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>, document.querySelector('#root'),
+  <MuiThemeProvider theme={theme}>
+    <Provider store={createStore(reducers, composeEnhancers(applyMiddleware(logger, thunk)))}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </MuiThemeProvider>, document.querySelector('#root'),
 );
